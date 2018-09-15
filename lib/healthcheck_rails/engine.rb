068ff34e4b1fa3c31598837cbbff8e44aa0cc3f3
Zenn <app.zenn@gmail.com>
2018-09-15 16:31:02 +0900

    add engine mounted_path method

--- lib/healthcheck_rails/engine.rb
+++ lib/healthcheck_rails/engine.rb
@@ -1,5 +1,14 @@
 module HealthcheckRails
   class Engine < ::Rails::Engine
     isolate_namespace HealthcheckRails
+
+    def self.mounted_path
+      @@mounted_path ||= begin
+        route = Rails.application.routes.routes.detect do |route|
+          route.app.app == self
+        end
+        route && route.path
+      end
+    end
   end
 end
